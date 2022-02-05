f3352bc9eecdd6c343f6cce6d0429c64a4cd719e
quenrythane <artur.babinski.g@gmail.com>
2022-02-05 16:08:48 +0100

    created empty board and now i am creating player board (with cards hide) and game board (with show cards)

--- memory.py
+++ memory.py
@@ -1,41 +1,78 @@
 import random
 
-# user write how how much card want?
-how_much_card = 0
-while not(0 < how_much_card <= 100):
-    try:
-        how_much_card = 20  # int(input('with how much card do you want to play?: '))
-    except TypeError:
-        print('Sorry, but the number must be between 1 and 100 and be integer \n')
 
-# inform player with number of card he will plays with
-if how_much_card % 2 != 0:
-    how_much_card -= 1
-    print("The number of cards couldn't be odd. I'll fix it for you and set this number to", how_much_card)
-print(f"You'll be playing with {how_much_card} cards")
+# user write how how much card want?
+def ask_user_how_much_card(int):
+    how_much_card = 0
+    while not(0 < how_much_card <= 100):  # użytkownik może wybrać od 1 do 100 kart. Jeśli wybierze inaczej, pętla będzie go dalej odpytywać
+        try:
+            how_much_card = int  # int(input('with how much card do you want to play?: '))
+        except TypeError:
+            print('Sorry, but the number must be between 1 and 100 and be integer \n')
+
+    # inform player with number of card he will plays with
+    if how_much_card % 2 != 0:
+        how_much_card -= 1
+        print("The number of cards couldn't be odd. I'll fix it for you and set this number to", how_much_card)
+    print(f"You'll be playing with {how_much_card} cards")
+    return how_much_card
+
+
+# prepare shuffled cards
+def prepare_shuffled_cards(how_much_card):
+    list_of_cards = [n for n in range(1, int(how_much_card/2)+1)] * 2
+    random.shuffle(list_of_cards)
+    return list_of_cards
 
 
 # count number of rows and columns (square shape)
-x = how_much_card ** (1/2)
-if x != int(x):
-    number_of_rows_and_columns = int(x) + 1
-else:
-    number_of_rows_and_columns = x
-print('number_of_rows_and_columns: ', number_of_rows_and_columns)
+def count_number_of_rows_and_columns(how_much_card):
+    x = how_much_card ** (1/2)
+    if x != int(x):
+        number_of_rows_and_columns = int(x) + 1
+    else:
+        number_of_rows_and_columns = x
+    return number_of_rows_and_columns
 
-""""""
-# prepare shuffled cards
-list_of_cards = [n for n in range(1, int(how_much_card/2)+1)] * 2
-random.shuffle(list_of_cards)
 
+"""
+dotąd działa    dotąd działa    dotąd działa    dotąd działa    
+dotąd działa    dotąd działa    dotąd działa    dotąd działa    
+dotąd działa    dotąd działa    dotąd działa    dotąd działa    
+"""
 
 # szykuje plansze
-def making_board(number_of_rows_and_columns):
-    global board
-    board = []
-    for x in range(number_of_rows_and_columns):
-        board.append([])
+def making_empty_board(number_of_rows_and_columns):
+    empty_board = []
+    [empty_board.append([]) for x in range(number_of_rows_and_columns)]
+    return empty_board
+
+
+def fill_board(number_of_rows_and_columns, empty_board, list_of_cards):
+    board = empty_board
+    for i, card in enumerate(list_of_cards):
+        board[i // number_of_rows_and_columns].append(card)
+
+    # tutaj sprawdzam czy ostatni z wierszy jest pusty i jeśli tak, to go usuwam (po co ma śmiecić?)
+    board.pop() if board[-1] == [] else board
+
     return board
+
+
+def making_user_board(number_of_rows_and_columns, list_of_cards):
+    global user_board
+    user_board = []
+    [user_board.append([]) for x in range(number_of_rows_and_columns)]
+    for i, card in enumerate(list_of_cards):
+        user_board[i // number_of_rows_and_columns].append('X')
+    return user_board
+
+
+"""
+pracuje nad tym     pracuje nad tym     pracuje nad tym     pracuje nad tym     pracuje nad tym     
+pracuje nad tym     pracuje nad tym     pracuje nad tym     pracuje nad tym     pracuje nad tym     
+pracuje nad tym     pracuje nad tym     pracuje nad tym     pracuje nad tym     pracuje nad tym     
+"""
 
 
 # rozkłada karty na planszy
@@ -44,18 +81,9 @@ def deals_the_cards_on_board(number_of_rows_and_columns, list_of_cards):
         board[i // number_of_rows_and_columns].append(card)
 
 
-def inside_the_board():
-    [print(board[i]) for i in range(len(board))]
-
-def show_the_board():
-    [print('x') for i in range(len(board))]
-
-
-
-making_board(number_of_rows_and_columns)
-deals_the_cards_on_board(number_of_rows_and_columns, list_of_cards)
-inside_the_board()
-show_the_board()
+# wyświetla plansze w konsoli
+def show_the_board(board_name):
+    [print(board[i]) for i in range(len(board_name))]
 
 
 
---
+++ memory_gra.py
@@ -0,0 +1,15 @@
+import memory
+
+
+x = 23  # int(input('with how much card do you want to play?: '))
+how_much_card = memory.ask_user_how_much_card(x)
+list_of_cards = memory.prepare_shuffled_cards(how_much_card)
+print('list_of_cards: ', list_of_cards)
+
+number_of_rows_and_columns = memory.count_number_of_rows_and_columns(how_much_card)
+print(f'number_of_rows_and_columns: {number_of_rows_and_columns}x{number_of_rows_and_columns}')
+
+empty_board = memory.making_empty_board(number_of_rows_and_columns)
+print('empty_board: ', empty_board)
+player_board = memory.fill_board(number_of_rows_and_columns, empty_board, list_of_cards)
+print('player_board: ', player_board)
